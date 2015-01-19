f13c193a988e3a6825996e1b28491b7d71979d6f
1000ch <shogo.sensui@gmail.com>
2015-01-19 21:39:12 +0900

    get information from dropped files

--- WebP/DropView.swift
+++ WebP/DropView.swift
@@ -39,12 +39,20 @@ class DropView: NSView, NSDraggingDestination {
     override func performDragOperation(sender: NSDraggingInfo) -> Bool {
         
         let pboard = sender.draggingPasteboard()
-        
-        println(pboard)
-        
         let draggedFilePaths = pboard.propertyListForType(NSFilenamesPboardType) as NSArray
         
         println(draggedFilePaths)
+        
+        let manager = NSFileManager.defaultManager()
+        var error: NSError?
+        for draggedFilePath in draggedFilePaths as [String] {
+            let attributes = manager.attributesOfFileSystemForPath(draggedFilePath, error: &error)
+            if error != nil {
+                println("エラーでましてん…")
+            } else {
+                println(attributes)
+            }
+        }
         
         return true
     }
