bf69e431b131fdc95c5699d1d65c466465f75db6
1000ch <shogo.sensui@gmail.com>
2015-01-20 13:04:21 +0900

    remove comment

--- WebP/TableView.swift
+++ WebP/TableView.swift
@@ -1,11 +1,3 @@
-//
-//  TableView.swift
-//  WebP
-//
-//  Created by A12993 on 2015/01/20.
-//  Copyright (c) 2015年 1000ch.net. All rights reserved.
-//
-
 import Cocoa
 
 class TableView: NSTableView {
