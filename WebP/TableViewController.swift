6de094f498eead8913bf1ac897f6b057da3e4eea
1000ch <shogo.sensui@gmail.com>
2015-01-20 13:53:57 +0900

    add dummy data

--- WebP/TableViewController.swift
+++ WebP/TableViewController.swift
@@ -9,4 +9,25 @@ class TableViewController: NSViewController, NSTableViewDelegate, NSTableViewDat
         // Do view setup here.
     }
     
+    func numberOfRowsInTableView() -> Int {
+        let numberOfRows: Int = getDataArray().count
+        return numberOfRows
+    }
+    
+    func tableView(tableView: TableView!, objectValueForTableColumn tableColumn: NSTableColumn!, row: Int) -> AnyObject! {
+        let headers: (AnyObject?) = getDataArray().objectAtIndex(row).objectForKey(tableColumn.identifier)
+        return headers
+    }
+    
+    func getDataArray () -> NSArray{
+        var dataArray: [NSDictionary] = [
+            ["FirstName": "Debasis", "LastName": "Das"],
+            ["FirstName": "Nishant", "LastName": "Singh"],
+            ["FirstName": "John", "LastName": "Doe"],
+            ["FirstName": "Jane", "LastName": "Doe"],
+            ["FirstName": "Mary", "LastName": "Jane"]
+        ];
+        println(dataArray);
+        return dataArray;
+    }
 }
