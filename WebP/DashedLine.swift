737f591119d14a0ec3a7c95847b0c060e7a26fee
1000ch <shogo.sensui@gmail.com>
2015-02-03 22:13:35 +0900

    draw dashed line

--- WebP/DashedLine.swift
+++ WebP/DashedLine.swift
@@ -3,11 +3,16 @@ import Cocoa
 class DashedLine: NSBox {
 
     override func drawRect(dirtyRect: NSRect) {
-        super.drawRect(dirtyRect)
-        
+        //super.drawRect(dirtyRect)
         
         NSColor.lightGrayColor().set()
-        NSRectFill(dirtyRect)
+        var path = NSBezierPath(rect: dirtyRect)
+        path.lineWidth = 10.0
+        
+        let pattern: [CGFloat] = [5.0, 5.0]
+        path.setLineDash(pattern, count: 2, phase: 0.0)
+        path.stroke()
+        
+        //NSRectFill(dirtyRect)
     }
-    
 }
