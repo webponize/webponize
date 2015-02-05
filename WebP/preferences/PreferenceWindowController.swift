2d8db64ed8be028ebdc78c1ef6ba88703275a431
1000ch <shogo.sensui@gmail.com>
2015-02-06 00:12:48 +0900

    remove unused window

--- WebP/preferences/PreferenceWindowController.swift
+++ WebP/preferences/PreferenceWindowController.swift
@@ -1,11 +1,7 @@
 import Cocoa
 
 class PreferenceWindowController: NSWindowController {
-    
-    @IBOutlet weak var preferenceWindow: PreferenceWindow!
-
     override func windowDidLoad() {
         super.windowDidLoad()
     }
-
 }
