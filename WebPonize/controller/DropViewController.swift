38d2ccb30e5cae25a53e4306ec5846100460c623
1000ch <shogo.sensui@gmail.com>
2015-05-21 22:34:30 +0900

    do operation at view controller

--- WebPonize/controller/DropViewController.swift
+++ WebPonize/controller/DropViewController.swift
@@ -2,16 +2,24 @@ import Cocoa
 
 class DropViewController: NSViewController {
 
+    let config: ApplicationConfig
+
     @IBOutlet weak var dropView: DropView!
     
     @IBOutlet weak var dropAreaView: DropAreaView!
     
     required init?(coder: NSCoder) {
+        config = AppDelegate.getAppDelegate().config
         super.init(coder: coder)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        dropView.onPerformDragOperation = { sender -> Void in
+            var filePaths = self.getDraggedFiles(sender)
+            self.convertFilesIntoWebP(filePaths)
+        }
         
         self.dropView.onDraggingEnteredHandler = { sender -> Void in
             self.dropAreaView.setHoverImage()
@@ -24,5 +32,32 @@ class DropViewController: NSViewController {
         self.dropView.onDraggingEndedHandler = { sender -> Void in
             self.dropAreaView.setImage()
         }
+    }
+    
+    func getDraggedFiles(draggingInfo: NSDraggingInfo) -> [String] {
+        let pboard = draggingInfo.draggingPasteboard()
+        return pboard.propertyListForType(NSFilenamesPboardType) as! [String]
+    }
+    
+    func convertFilesIntoWebP(filePaths: [String]) {
+
+        let compressionLevel = config.getCompressionLevel()
+        let isLossless = config.getIsLossless()
+        let isNoAlpha = config.getIsNoAlpha()
+
+        var queue = NSOperationQueue()
+        queue.maxConcurrentOperationCount = 1
+        
+        for filePath in filePaths {
+            
+            let operation = ConvertOperation(
+                filePath: filePath,
+                compressionLevel: compressionLevel,
+                isLossless: isLossless,
+                isNoAlpha: isNoAlpha)
+            
+            queue.addOperation(operation)
+        }
+
     }
 }
--- WebPonize/view/DropView.swift
+++ WebPonize/view/DropView.swift
@@ -1,16 +1,8 @@
 import Cocoa
 
 class DropView: NSView, NSDraggingDestination {
-    
-    var config: ApplicationConfig
-        
-    override init(frame: NSRect) {
-        config = AppDelegate.getAppDelegate().config
-        super.init(frame: frame)
-    }
 
     required init?(coder: NSCoder) {
-        config = AppDelegate.getAppDelegate().config
         super.init(coder: coder)
     }
 
@@ -22,38 +14,11 @@ class DropView: NSView, NSDraggingDestination {
             NSColorPboardType,
             NSFilenamesPboardType
         ] as [AnyObject]
-        //let acceptDragTypes = NSImage.imageTypes()
 
         registerForDraggedTypes(acceptDragTypes)
     }
     
-    override func performDragOperation(sender: NSDraggingInfo) -> Bool {
-        
-        let compressionLevel = config.getCompressionLevel()
-        let isLossless = config.getIsLossless()
-        let isNoAlpha = config.getIsNoAlpha()
-        
-        // get dragged files' path
-        let pboard = sender.draggingPasteboard()
-        let filePaths = pboard.propertyListForType(NSFilenamesPboardType) as! [String]
-
-        // create operation & add into queue
-        var queue = NSOperationQueue()
-        queue.maxConcurrentOperationCount = 1
-
-        for filePath in filePaths {
-
-            let operation = ConvertOperation(
-                filePath: filePath,
-                compressionLevel: compressionLevel,
-                isLossless: isLossless,
-                isNoAlpha: isNoAlpha)
-
-            queue.addOperation(operation)
-        }
-        
-        return true
-    }
+    var onPerformDragOperation: ((sender: NSDraggingInfo) -> Void)?
     
     var onDraggingEnteredHandler: ((sender: NSDraggingInfo) -> Void)?
     
@@ -61,19 +26,21 @@ class DropView: NSView, NSDraggingDestination {
     
     var onDraggingEndedHandler: ((sender: NSDraggingInfo) -> Void)?
     
+    override func performDragOperation(sender: NSDraggingInfo) -> Bool {
+        onPerformDragOperation?(sender: sender)
+        return true
+    }
+    
     override func draggingEntered(sender: NSDraggingInfo) -> NSDragOperation  {
-        // delegate to view controller
         onDraggingEnteredHandler?(sender: sender)
         return NSDragOperation.Copy
     }
     
     override func draggingExited(sender: NSDraggingInfo?) {
-        // delegate to view controller
         onDraggingExitedHandler?(sender: sender!)
     }
 
     override func draggingEnded(sender: NSDraggingInfo?) {
-        // delegate to view controller
         onDraggingEndedHandler?(sender: sender!)
     }
 }
