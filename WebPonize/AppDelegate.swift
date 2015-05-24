707331abcea372b374b817fa6729c0135768e1cb
1000ch <shogo.sensui@gmail.com>
2015-05-24 09:18:59 +0900

    re-open window on click dock icon

--- WebPonize/AppDelegate.swift
+++ WebPonize/AppDelegate.swift
@@ -9,8 +9,23 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     static var operationQueue = NSOperationQueue()
     
+    var mainWindow: NSWindow!
+    
     override init() {
         super.init()
         AppDelegate.operationQueue.maxConcurrentOperationCount = 1
+    }
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        mainWindow = NSApplication.sharedApplication().windows[0] as! NSWindow
+    }
+    
+    func applicationShouldHandleReopen(sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        
+        if !flag {
+            mainWindow.makeKeyAndOrderFront(nil)
+        }
+        
+        return true
     }
 }
