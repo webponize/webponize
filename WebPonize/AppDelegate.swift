3c571389720b6d5eb505117aff36248ff5afaafd
1000ch <shogo.sensui@gmail.com>
2018-01-03 22:03:32 +0900

    Make mainWindow optional

--- WebPonize/AppDelegate.swift
+++ WebPonize/AppDelegate.swift
@@ -6,7 +6,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     static var statusList: [Status] = []
     static var queue = OperationQueue()
     
-    var mainWindow: NSWindow!
+    var mainWindow: NSWindow?
     
     override init() {
         super.init()
@@ -15,12 +15,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        mainWindow = NSApplication.shared.windows[0] 
+        mainWindow = NSApplication.shared.windows.first
     }
     
     func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
         if !flag {
-            mainWindow.makeKeyAndOrderFront(nil)
+            mainWindow?.makeKeyAndOrderFront(nil)
         }
         
         return true
