1dd41bd72ff2b13a03e4d58cda2ab462c219397c
1000ch <shogo.sensui@gmail.com>
2018-01-03 16:37:57 +0900

    Catch convert error

--- WebPonize/Helper/ConvertOperation.swift
+++ WebPonize/Helper/ConvertOperation.swift
@@ -31,6 +31,10 @@ class ConvertOperation: Operation {
             return
         }
 
+        guard let status = AppDelegate.statusList.filter({ $0.uuid == uuid }).first else {
+            return
+        }
+
         let encoder = WebPEncoder()
         var config = WebPEncoderConfig.preset(WebPEncoderConfig.Preset.default, quality: AppDelegate.appConfig.quality)
         config.method = AppDelegate.appConfig.speed
@@ -43,18 +47,16 @@ class ConvertOperation: Operation {
         config.alphaFiltering = AppDelegate.appConfig.alphaFiltering
         config.alphaCompression = AppDelegate.appConfig.alphaCompression
 
-        let input = try! Data(contentsOf: fileURL)
-        let output = try! encoder.encode(NSImage(data: input)!, config: config)
-        NSData(data: output).write(to: folder.appendingPathComponent(fileName), atomically: true)
-                
-        let status = AppDelegate.statusList.filter({ $0.uuid == uuid }).first
-        status?.beforeByte = input.count
-        status?.afterByte = output.count
-
-        if input.count > output.count {
-            status?.status = StatusType.finished
-        } else {
-            status?.status = StatusType.error
+        do {
+            let input = try! Data(contentsOf: fileURL)
+            let output = try! encoder.encode(NSImage(data: input)!, config: config)
+            NSData(data: output).write(to: folder.appendingPathComponent(fileName), atomically: true)
+            
+            status.beforeByte = input.count
+            status.afterByte = output.count
+            status.status = StatusType.finished
+        } catch {
+            status.status = StatusType.error
         }
     }
 }
