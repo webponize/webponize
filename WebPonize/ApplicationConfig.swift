faf8d6940e1f56522a2737f3a9eb2a78c8c75db5
1000ch <shogo.sensui@gmail.com>
2018-01-05 01:04:33 +0900

    Add tests

--- WebPonize/ApplicationConfig.swift
+++ WebPonize/ApplicationConfig.swift
@@ -16,66 +16,106 @@ class ApplicationConfig: Preference {
     
     var quality: Float = 80 {
         willSet {
+            if newValue < 0 || 100 < newValue {
+                return
+            }
+
             set(Key.quality.rawValue, value: newValue as AnyObject)
         }
     }
     
     var speed: Int = 0 {
         willSet {
+            if newValue < 0 || 6 < newValue {
+                return
+            }
+
             set(Key.speed.rawValue, value: newValue as AnyObject)
         }
     }
     
     var lossless: Int = 0 {
         willSet {
+            if newValue != 0 && newValue != 1 {
+                return
+            }
+
             set(Key.lossless.rawValue, value: newValue as AnyObject)
         }
     }
     
     var filterStrength: Int = 0 {
         willSet {
+            if newValue < 0 || 100 < newValue {
+                return
+            }
+
             set(Key.filterStrength.rawValue, value: newValue as AnyObject)
         }
     }
     
     var filterSharpness: Int = 0 {
         willSet {
+            if newValue < 0 || 7 < newValue {
+                return
+            }
+
             set(Key.filterSharpness.rawValue, value: newValue as AnyObject)
         }
     }
     
     var filterType: Int = 0 {
         willSet {
+            if newValue != 0 && newValue != 1 {
+                return
+            }
+
             set(Key.filterType.rawValue, value: newValue as AnyObject)
         }
     }
     
     var autoFilter: Int = 0 {
         willSet {
+            if newValue != 0 && newValue != 1 {
+                return
+            }
+
             set(Key.autoFilter.rawValue, value: newValue as AnyObject)
         }
     }
     
     var alphaQuality: Int = 100 {
         willSet {
+            if newValue < 0 || 100 < newValue {
+                return
+            }
+
             set(Key.alphaQuality.rawValue, value: newValue as AnyObject)
         }
     }
     
     var alphaFiltering: Int = 1 {
         willSet {
+            if newValue != 0 && newValue != 1 && newValue != 2 {
+                return
+            }
+
             set(Key.alphaFiltering.rawValue, value: newValue as AnyObject)
         }
     }
     
     var alphaCompression: Int = 1 {
         willSet {
+            if newValue != 0 && newValue != 1 {
+                return
+            }
+
             set(Key.alphaCompression.rawValue, value: newValue as AnyObject)
         }
     }
     
-    init() {
-        super.init(applicationId: "net.1000ch.WebPonize")
+    override init(applicationId: String) {
+        super.init(applicationId: applicationId)
         
         quality = has(Key.quality.rawValue) ? get(Key.quality.rawValue) as! Float : quality
         speed = has(Key.speed.rawValue) ? get(Key.speed.rawValue) as! Int : speed
@@ -89,5 +129,9 @@ class ApplicationConfig: Preference {
         alphaQuality = has(Key.alphaQuality.rawValue) ? get(Key.alphaQuality.rawValue) as! Int : alphaQuality
         alphaFiltering = has(Key.alphaFiltering.rawValue) ? get(Key.alphaFiltering.rawValue) as! Int : alphaFiltering
         alphaCompression = has(Key.alphaCompression.rawValue) ? get(Key.alphaCompression.rawValue) as! Int : alphaCompression
+    }
+    
+    convenience init() {
+        self.init(applicationId: "net.1000ch.WebPonize")
     }
 }
